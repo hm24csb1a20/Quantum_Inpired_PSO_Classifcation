5d5e0f5af320f73cef838fd4a522022b7570f462
hm24csb1a20 <hm24csb1a20@student.nitw.ac.in>
2025-10-22 12:50:41 +0530

    updated the code till acceleration is calcuated

--- logistic_base_functions.py
+++ logistic_base_functions.py
@@ -22,23 +22,54 @@ def fitness_function(X_train,Y_train,
     the element here is the feature map for the featrues to use
     """
     if(element!=None):
-        X_train = X_train[:,element]
-        X_test = X_test[:,element]
-        nfeat = np.sum(element)
+        X_train = X_train[:, element.astype(bool)]
+        X_test = X_test[:, element.astype(bool)]
+        n_feat = np.sum(element)
     else:
         n_feat = X_train.shape[1] #the total no of columns
-    model = LogisticRegression(max_iter=1e9)
+    model = LogisticRegression(max_iter=int(1e9))
     model.fit(X_train,Y_train)
     Y_pred = model.predict(X_test)
     acc = accuracy_score(Y_test,Y_pred)
-
+    acc = max(acc, 1e-10)  # prevent log(0)
     fitness = (
-         alpha * np.log(acc)-
+         alpha * np.log(acc)
          - (1 - alpha) * (n_feat / np.sqrt(X_train.shape[1])) * acc * (1 + np.sin(golden_ratio))
     )
     return fitness,acc,n_feat
 
-    
+def make_fitness_array(X_train,Y_train,X_test, Y_test,population):
+    """
+    to return the fitness of the entire population """
+    return np.array([fitness_function(X_train,Y_train,X_test, Y_test,i) for i in population])
+
+def best_worst_fitness(fitnesses):
+    """return the best and the worst perfoming element fitness values"""
+    return max(fitnesses),min(fitnesses)
+
+def computeMi_Mbest(fitnesses,fbest,fworst):
+    """returns the array of masses of each elemnet
+    the best Mi 
+    the best mi (normalized Mi)"""
+    Mi = np.array([abs(i - fbest) / (fbest - fworst) for i in fitnesses])
+    mi = Mi / np.sum(Mi)
+    return Mi,max(Mi),mi
+
+def computeOmega(iteration, max_iter, omega_max=1.0, omega_min=0.4):
+    """linearly decaying omega (inertia/contraction factor)"""
+    return omega_max - (omega_max - omega_min) * (iteration / max_iter)
+
+def computeAcceleration(population, pbest, mbest, omega, mi):
+    r1 = np.random.rand(*population.shape)
+    r2 = np.random.rand(*population.shape)
+
+    term1 = omega * r1 * (pbest - population)
+    term2 = (1 - omega) * r2 * (mbest - population)
+
+    acceleration = (term1 + term2) * mi[:, np.newaxis]  # scale by normalized mass
+    return acceleration
+
+
 
 if __name__ =='__main__':
     # 
