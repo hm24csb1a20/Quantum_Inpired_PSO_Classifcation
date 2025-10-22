f31060d03f10590e1ea75509ebe394cfb262c8df
hm24csb1a20 <hm24csb1a20@student.nitw.ac.in>
2025-10-22 21:53:31 +0530

    was chaing the whole dataset in each iteration changed that to only change locally

--- randomforest_base_functions.py
+++ randomforest_base_functions.py
@@ -7,12 +7,13 @@ from sklearn.ensemble import RandomForestClassifier
 import os
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import cross_val_score, StratifiedKFold
 
 # global hyperparameters 
 alpha =0.7
 max_iter = int(1e3/2)
 g0=35
-popsize = 30
+popsize = 50
 
 def initialize_population(popsize,n):
     """makes the poulation of popsize 
@@ -21,7 +22,6 @@ def initialize_population(popsize,n):
     return np.random.randint(0, 2, size=(popsize, n))
 
 def fitness_function(X_train,Y_train,
-                     X_test, Y_test,
                      element,
                      alpha =0.8,
                      golden_ratio = 1.618):
@@ -31,23 +31,27 @@ def fitness_function(X_train,Y_train,
     the element here is the feature map for the featrues to use
     """
     if element is not None:
-        X_train = X_train[:, element.astype(bool)]
-        X_test = X_test[:, element.astype(bool)]
+        X_train_subset = X_train[:, element.astype(bool)]
+        # X_test_subset = X_test[:, element.astype(bool)]
         n_feat = np.sum(element)
     else:
+        X_train_subset = X_train
         n_feat = X_train.shape[1] #the total no of columns
-    model = RandomForestClassifier(n_estimators=75, max_depth=None, random_state=34)
-    model.fit(X_train, Y_train)
-    Y_pred = model.predict(X_test)
-    acc = accuracy_score(Y_test,Y_pred)
-    acc = max(acc, 1e-10)  # prevent log(0)
+    
+    # model = RandomForestClassifier(n_estimators=75, max_depth=None, random_state=2)
+    model = LogisticRegression(solver='liblinear', random_state=2)
+    skf = StratifiedKFold(n_splits=3, shuffle=True, random_state=76)
+    accuracies = cross_val_score(model, X_train_subset, Y_train, cv=skf, scoring='accuracy')
+    acc = np.mean(accuracies) # Use the average accuracy
+
+    acc = max(acc, 1e-10)
     fitness = (
          alpha * np.log(acc)
          - (1 - alpha) * (n_feat / np.sqrt(X_train.shape[1]))  * (1 + np.sin(golden_ratio))
     )
     return fitness,acc,n_feat
 
-def make_fitness_array(X_train,Y_train,X_test, Y_test,population):
+def make_fitness_array(X_train,Y_train,population):
     """
     to return the fitness of the entire population """
     return np.array([fitness_function(X_train,Y_train,X_test, Y_test,i) for i in population])
@@ -64,7 +68,8 @@ def computeMi_Mbest(fitnesses,fbest,fworst):
     """returns the array of masses of each elemnet
     the best Mi 
     the best mi (normalized Mi)"""
-    Mi = np.array([abs(i - fbest) / (fbest - fworst) for i in fitnesses])
+    # the 1e-10 added to make sure if fbest==fworst no division by 10 error
+    Mi = np.array([abs(i - fbest) / ((fbest - fworst)+int(1e-10)) for i in fitnesses])
     mi = Mi / np.sum(Mi)
     return Mi,max(Mi),mi
 
@@ -96,7 +101,6 @@ def compute_gravity_force(max_iter,
     return g0*np.exp(-1*alpha*i/max_iter)
 
 def qigpso_feature_selection(X_train,Y_train,
-                             X_test,Y_test,
                              popsize =20, 
                              alpha =0.1,
                              max_iter=int(1e2),
@@ -127,7 +131,7 @@ def qigpso_feature_selection(X_train,Y_train,
         # compute mean best position weighted by mi
         mbest = np.sum(pbest * mi[:, np.newaxis], axis=0)
 
-        acc = compute_acc(population,pbest,mbest,omega,mi)*3
+        acc = compute_acc(population,pbest,mbest,omega,mi)*1.2
 
         # doing the quantum gravaiton position update 
         # making some random data
@@ -137,7 +141,9 @@ def qigpso_feature_selection(X_train,Y_train,
         # remvoign the data values which arent [0,1]
         new_population = np.clip(new_population, 0, 1)
         # make teh data a binary 0 or 1
-        new_population = (new_population > 0.4).astype(int)
+        # changign the constant 0.5 to sigmoid
+        sigmoid = 1 / (1 + np.exp(-new_population))
+        new_population = (np.random.rand(*population.shape) > sigmoid).astype(int)
 
 
         # evaluating the fitness vales for this data
@@ -145,9 +151,9 @@ def qigpso_feature_selection(X_train,Y_train,
         new_fitnesses= np.array([f[0]for f in new_fitness_raw])
         new_fitnesses = new_fitnesses.flatten() 
         # make random flips 
-        # flip_prob = 0.05
-        # rand_flip = np.random.rand(*new_population.shape) < flip_prob
-        # new_population[rand_flip] = 1 - new_population[rand_flip]
+        flip_prob = 0.02
+        rand_flip = np.random.rand(*new_population.shape) < flip_prob
+        new_population[rand_flip] = 1 - new_population[rand_flip]
 
 
         # make the changes wherever the fitness gets better
@@ -190,9 +196,9 @@ if __name__ =='__main__':
     X_values = X.values
 
     X_train, X_test, Y_train, Y_test = train_test_split(X_values, y, test_size=0.2,
-                                                    random_state=22, stratify=y)
+                                                    random_state=11, stratify=y)
 
-    best_features, best_fitness = qigpso_feature_selection(X_train, Y_train, X_test, Y_test,popsize,alpha,max_iter,g0)
+    best_features, best_fitness = qigpso_feature_selection(X_train, Y_train, popsize,alpha,max_iter,g0)
     
     # mpa indices to column names
     selected_feature_names = feature_names[np.where(best_features == 1)[0]]
@@ -207,7 +213,7 @@ if __name__ =='__main__':
     # doing the classificaton
     model = RandomForestClassifier(
         n_estimators=200,
-        random_state=42
+        random_state=55
     )
     model.fit(X_train_selected,Y_train)
     Y_pred = model.predict(X_test_selected)
